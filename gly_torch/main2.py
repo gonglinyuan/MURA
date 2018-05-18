825342a204e35254363100fd5d68293a6301aabb
Gong Linyuan <gonglinyuan@hotmail.com>
2018-05-18 12:59:22 +0800

    test models

--- gly_torch/main2.py
+++ gly_torch/main2.py
@@ -10,8 +10,8 @@ from data_augmentation import DataTransform
 
 
 def main():
-    # runTest("../../trained_models/20180504-003410/m-20180504-003410.pth.tar")
-    run_train()
+    runTest("../../trained_models/20180515-212033/m-20180515-212033")
+    # run_train()
 
 
 def run_train():
@@ -62,6 +62,7 @@ def run_train():
         optimizer_fn=optimizers.adam_optimizers
     )
 
+    print('NN architecture = ', model_name)
     print("using data transforms: " + str(data_transform))
 
     print('Testing the model with best valid-loss')
@@ -89,38 +90,53 @@ def run_train():
     )
 
 
-# def runTest(path_model):
-#     path_data_valid = '../../MURA_valid1_keras'
-#     model_name = 'INCEPTIONRESNETV2'
-#     model_pretrained = True
-#     # batch_size = 16
-#     batch_size = 6
-#     # batch_size = 10
-#     img_size = 341
-#     crop_size = 299
-#     target_mean = 0.5
-#     target_std = 0.5
-#
-#     device = None
-#     opts, _ = getopt.getopt(sys.argv[1:], "d:", ["device="])
-#     for opt, arg in opts:
-#         if opt in ("-d", "--device") and torch.cuda.is_available():
-#             device = torch.device("cuda:" + str(arg))
-#     if device is None:
-#         print("GPU not found! Using CPU!")
-#         device = torch.device("cpu")
-#
-#     print('Testing the trained model')
-#     train.test(
-#         path_data=path_data_valid,
-#         path_model=path_model,
-#         model_name=model_name,
-#         model_pretrained=model_pretrained,
-#         batch_size=batch_size,
-#         device=device,
-#         transform=data_augmentation.valid_transform(img_size=img_size, crop_size=crop_size,
-#                                                     target_mean=target_mean, target_std=target_std)
-#     )
+def runTest(path_model):
+    model_name = 'DENSENET201'
+    model_pretrained = True
+    path_data_valid = '../../MURA_valid1_keras'
+    batch_size = 16
+    img_size = 256
+    crop_size = 224
+    target_mean = 0.0
+    target_std = 1.0
+
+    data_transform = DataTransform(revised=True, aug="slight", no_bg=True, pad=True, to_rgb=False)
+    data_transform_valid = data_transform.get_valid(img_size=img_size, crop_size=crop_size, target_mean=target_mean,
+                                                    target_std=target_std)
+
+    device = None
+    opts, _ = getopt.getopt(sys.argv[1:], "d:", ["device="])
+    for opt, arg in opts:
+        if opt in ("-d", "--device") and torch.cuda.is_available():
+            device = torch.device("cuda:" + str(arg))
+    if device is None:
+        print("GPU not found! Using CPU!")
+        device = torch.device("cpu")
+
+    print('NN architecture = ', model_name)
+    print("using data transforms: " + str(data_transform))
+
+    print('Testing the model with best valid-loss')
+    train.test(
+        path_data=path_data_valid,
+        path_model=path_model + "-L",
+        model_name=model_name,
+        model_pretrained=model_pretrained,
+        batch_size=batch_size,
+        device=device,
+        transform=data_transform_valid
+    )
+
+    print('Testing the model with best valid-auroc')
+    train.test(
+        path_data=path_data_valid,
+        path_model=path_model + "-A",
+        model_name=model_name,
+        model_pretrained=model_pretrained,
+        batch_size=batch_size,
+        device=device,
+        transform=data_transform_valid
+    )
 
 
 # --------------------------------------------------------------------------------
