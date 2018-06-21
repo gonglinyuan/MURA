1db6d4557311cafa660467084327b136fb37de98
Gong Linyuan <gonglinyuan@hotmail.com>
2018-06-21 12:33:58 +0800

    try pnasnet, densenet161, densenet201, vgg19bnlarge

--- gly_torch/main2.py
+++ gly_torch/main2.py
@@ -17,21 +17,21 @@ def main():
 
 def run_train():
     timestamp = time.strftime("%Y%m%d") + '-' + time.strftime("%H%M%S")
-    model_name = 'PNASNET'
+    model_name = 'DENSENET161-LARGE3'
     model_pretrained = True
     path_data_train = '../../MURA_trainval_keras'
     path_data_valid = '../../MURA_valid1_keras'
     path_log = '../../trained_models/' + timestamp + '/tb'
     # batch_size = 16
-    batch_size = 6
+    batch_size = 16
     epoch_num = 100
-    img_size = 378
-    crop_size = 331
-    target_mean = 0.5
-    target_std = 0.5
+    img_size = 366
+    crop_size = 320
+    target_mean = np.array([0.485, 0.456, 0.406])
+    target_std = np.array([0.229, 0.224, 0.225])
     path_model = '../../trained_models/' + timestamp + '/m-' + timestamp
 
-    data_transform = DataTransform(no_bg=True, pad=True)
+    data_transform = DataTransform(aug="rot30", no_bg=True, pad=True)
     data_transform_train = data_transform.get_train(img_size=img_size, crop_size=crop_size, target_mean=target_mean,
                                                     target_std=target_std)
     data_transform_valid = data_transform.get_valid(img_size=img_size, crop_size=crop_size, target_mean=target_mean,
