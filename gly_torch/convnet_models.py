84298cd99200c357142ed305fea3df094787cbfa
Gong Linyuan <gonglinyuan@hotmail.com>
2018-06-27 08:33:44 +0800

    fix

--- gly_torch/convnet_models.py
+++ gly_torch/convnet_models.py
@@ -71,7 +71,7 @@ class ConvnetModel(nn.Module):
         elif model_name == 'INCEPTIONV3':
             self.convnet = MODELS[model_name](pretrained='imagenet', transform_input=False, aux_logits=False)
         elif model_name.startswith('DUAL'):
-            if model_name.endswith('5k'):
+            if model_name.endswith('5k') or model_name.endswith('5k-LARGE'):
                 self.convnet = MODELS[model_name](pretrained='imagenet+5k')
             else:
                 self.convnet = MODELS[model_name](pretrained='imagenet')
