6c56cda50c5b4feb06dce45d4e1fba23af02a20f
Gong Linyuan <gonglinyuan@hotmail.com>
2018-06-30 13:15:59 +0800

    try smaller learning rate

--- gly_torch/optimizers.py
+++ gly_torch/optimizers.py
@@ -18,3 +18,9 @@ def nsgd_optimizers(parameters):
     optimizer = optim.SGD(parameters, lr=0.001, momentum=0.9, weight_decay=1e-5, nesterov=True)
     scheduler = ReduceLROnPlateau(optimizer, factor=0.1, patience=5, mode='min', verbose=True)
     return optimizer, scheduler
+
+
+def adam_optimizers_small(parameters):
+    optimizer = optim.Adam(parameters, lr=0.00001, betas=(0.9, 0.999), eps=1e-08, weight_decay=1e-5)
+    scheduler = ReduceLROnPlateau(optimizer, factor=0.1, patience=5, mode='min', verbose=True)
+    return optimizer, scheduler
