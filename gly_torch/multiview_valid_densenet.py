0ab4a5a64bb2ee152180d88f9d908d327f4caf06
Gong Linyuan <gonglinyuan@hotmail.com>
2018-07-03 15:07:22 +0800

    fix

--- gly_torch/multiview_valid_densenet.py
+++ gly_torch/multiview_valid_densenet.py
@@ -21,8 +21,8 @@ def run_test(path_model):
     batch_size = 20
     img_size = 293
     crop_size = 256
-    target_mean = 0.0
-    target_std = 1.0
+    target_mean = 0.456
+    target_std = 0.225
     data_transform = DataTransform(no_bg=True, pad=True)
     data_transform_valid = data_transform.get_valid(img_size=img_size, crop_size=crop_size, target_mean=target_mean,
                                                     target_std=target_std)
