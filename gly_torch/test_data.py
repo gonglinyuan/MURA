531cc577bc7105e39b3b5a72f2b7f08c04fd24fd
Gong Linyuan <gonglinyuan@hotmail.com>
2018-07-03 21:41:29 +0800

    debug

--- gly_torch/test_data.py
+++ gly_torch/test_data.py
@@ -4,6 +4,7 @@ from PIL import Image
 
 
 def img_loader(path):
+    print(path)
     with open(path, 'rb') as f:
         img = Image.open(f)
         return img.convert('RGB')
@@ -16,13 +17,10 @@ class TestData(torch.utils.data.Dataset):
         self.transform = transform
 
     def __getitem__(self, index):
-        print(2, index)
         path = self.image_paths[index]
         sample = self.loader(path)
-        print(3, index)
         if self.transform is not None:
             sample = self.transform(sample)
-        print(4, index)
         return sample, '/'.join(path.split('/')[:-1])
 
     def __len__(self):
