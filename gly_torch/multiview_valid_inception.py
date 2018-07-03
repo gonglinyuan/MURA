2be48ac460530ab1d13993634fbcbd5b224aa407
Gong Linyuan <gonglinyuan@hotmail.com>
2018-07-03 14:38:07 +0800

    fix

--- gly_torch/multiview_valid_inception.py
+++ gly_torch/multiview_valid_inception.py
@@ -10,7 +10,7 @@ from data_augmentation import DataTransform
 
 
 def main():
-    run_test("../../trained_models/03958-09035-inceptionv4large-adam-augslightnobgpad/m-20180601-025507")
+    run_test("../../trained_models/03958-09035-inceptionv4large-adam-augslightnobgpad/m-20180601-025449")
 
 
 def run_test(path_model):
