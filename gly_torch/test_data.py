04961a34c6df102cdf36b5fb47e713f38834b838
Gong Linyuan <gonglinyuan@hotmail.com>
2018-07-03 21:33:00 +0800

    debug

--- gly_torch/test_data.py
+++ gly_torch/test_data.py
@@ -19,8 +19,10 @@ class TestData(torch.utils.data.Dataset):
         print(2, index)
         path = self.image_paths[index]
         sample = self.loader(path)
+        print(3, index)
         if self.transform is not None:
             sample = self.transform(sample)
+        print(4, index)
         return sample, '/'.join(path.split('/')[:-1])
 
     def __len__(self):
