6325561e8fad90c47551f8da998599c7b26d9d9a
gonglinyuan <gonglinyuan@hotmail.com>
2018-07-28 14:01:50 +0800

    try different optimizers for densenet201

--- gly_torch_new/generate_config.py
+++ gly_torch_new/generate_config.py
@@ -7,19 +7,20 @@ if __name__ == "__main__":
         "transform": DataTransform(
             no_bg=True,
             pad=True,
+            aug_rotate=30,
             flip_h="random",
             crop_mode="random"
         ),
-        "batch_size": 16,
-        "optimizer_name": "adam",
-        "learning_rate": 1e-4,
+        "batch_size": 20,
+        "optimizer_name": "sgd",
+        "learning_rate": 1e-3,
         "weight_decay": 1e-5,
         "is_nesterov": False,
         "epoch_num": 60
     }
 
     config_valid = {
-        "model_name": "DenseNet169",
+        "model_name": "DenseNet201",
         "img_size": 366,
         "crop_size": 320,
         "transform": DataTransform(
@@ -35,5 +36,5 @@ if __name__ == "__main__":
         "valid": config_valid
     }
 
-    with open("configs/config001.yaml", "w") as f:
+    with open("configs/config006.yaml", "w") as f:
         yaml.dump(config, f, default_flow_style=False)
