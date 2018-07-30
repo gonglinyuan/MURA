50c9afc6622640fa88192baadffa4d1da6990bfc
gonglinyuan <gonglinyuan@hotmail.com>
2018-07-30 14:32:31 +0800

    train densenet201 densenet161

--- gly_torch_new/generate_config.py
+++ gly_torch_new/generate_config.py
@@ -8,23 +8,23 @@ if __name__ == "__main__":
         "transform": DataTransform(
             no_bg=True,
             pad=True,
-            aug_rotate=np.random.uniform(0.0, 30.0),
+            aug_rotate=30,
             flip_h="random",
             crop_mode="random",
-            random_crop_factor=np.random.uniform(0.08, 0.5)
+            random_crop_factor=0.08
         ),
         "batch_size": 20,
-        "optimizer_name": "adam",
-        "learning_rate": 1e-4,
-        "weight_decay": np.power(10.0, np.random.uniform(-6.0, -3.0)),
+        "optimizer_name": "sgd",
+        "learning_rate": 3e-4,
+        "weight_decay": 1e-5,
         "is_nesterov": False,
-        "beta1": np.random.uniform(0.5, 0.9),
+        "beta1": 0.9,
         "beta2": 0.999,
         "epoch_num": 60
     }
 
     config_valid = {
-        "model_name": "DenseNet201",
+        "model_name": "DenseNet161",
         "img_size": 366,
         "crop_size": 320,
         "transform": DataTransform(
@@ -40,5 +40,5 @@ if __name__ == "__main__":
         "valid": config_valid
     }
 
-    with open("configs/config016.yaml", "w") as f:
+    with open("configs/config023.yaml", "w") as f:
         yaml.dump(config, f, default_flow_style=False)
