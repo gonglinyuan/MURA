265a914deb21eaf98f11a063d3129b5e337bef78
Gong Linyuan <gonglinyuan@hotmail.com>
2018-08-14 09:26:09 +0800

    train inceptionv4 and dpn107

--- gly_torch_new/generate_config.py
+++ gly_torch_new/generate_config.py
@@ -9,27 +9,27 @@ if __name__ == "__main__":
             no_bg=True,
             pad=True,
             aug_rotate=20,
-            aug_shear=10,
+            aug_shear=0,
             flip_h="random",
             crop_mode="random",
             random_crop_factor=0.08
         ),
-        "batch_size": 20,
+        "batch_size": 16,
         "optimizer_name": "sgd",
-        "learning_rate": 1e-6 * (0.01 / 1e-6) ** (71 / 80),
-        "differential_lr": 1,
+        "learning_rate": 1e-6 * (0.01 / 1e-6) ** (42 / 80),
+        "differential_lr": 10,
         "weight_decay": 0,
         "is_nesterov": False,
         "beta1": 0.9,
         "beta2": 0.999,
         "epoch_num": 60,
-        "scheduling": "Cosine"
+        "scheduling": "Adaptive"
     }
 
     config_valid = {
-        "model_name": "InceptionV4",
-        "img_size": 378,
-        "crop_size": 331,
+        "model_name": "DPN107",
+        "img_size": 256,
+        "crop_size": 224,
         "transform": DataTransform(
             no_bg=True,
             pad=True,
@@ -43,5 +43,5 @@ if __name__ == "__main__":
         "valid": config_valid
     }
 
-    with open("configs/config095.yaml", "w") as f:
+    with open("configs/config107.yaml", "w") as f:
         yaml.dump(config, f, default_flow_style=False)
